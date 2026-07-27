20732fac5aad1742a359f2976fb2baf0fcf4a2f5
pax-ad <ad007scrutinize@gmail.com>
2026-07-27 23:24:17 +0530

    added arrays

---
+++ Array/Print.java
@@ -0,0 +1,12 @@
+package Array;
+
+import java.util.Arrays;
+
+public class Print {
+    
+    //printing an array
+    public static void main(String[] args) {
+        int [] arr = {1,2,6,3,4,5,};
+        System.out.println(Arrays.toString(arr));
+    }
+}
