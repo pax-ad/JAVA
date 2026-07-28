b34ab9a205eb66ff4b40aa05165f09ba1f319c18
pax-ad <ad007scrutinize@gmail.com>
2026-07-28 21:56:50 +0530

    Class concept

--- OOPs/Class.java
+++ OOPs/Class.java
@@ -1,5 +1,7 @@
 package OOPs;
 
+//A class is a blueprint or template used to create objects. It defines the properties (fields) and behaviours (methods) that objects of that class will have.
+
 public class Class {
     
     //program showcasing what is class
