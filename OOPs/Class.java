46fc7c232ef214247b804d02b1776d8b3140e2b8
pax-ad <ad007scrutinize@gmail.com>
2026-07-28 21:50:00 +0530

    OOPs concepts

---
+++ OOPs/Class.java
@@ -0,0 +1,19 @@
+package OOPs;
+
+public class Class {
+    
+    //program showcasing what is class
+
+}
+
+class student {
+
+    //properties 
+    String Name;
+    int age;
+
+    //Method 
+    void study(){
+        System.out.println(Name + "is studing");
+    }
+}
