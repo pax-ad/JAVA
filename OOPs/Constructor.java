321196f75439902b45afd5cfff117ad81462154a
pax-ad <ad007scrutinize@gmail.com>
2026-07-28 22:00:26 +0530

    constructor explained

---
+++ OOPs/Constructor.java
@@ -0,0 +1,31 @@
+package OOPs;
+
+public class Constructor {
+    
+    /*A constructor is a special method that is automatically called when an object is created. It is used to initialise the object's data.
+
+Rules
+Constructor name must be the same as the class name.
+It has no return type (not even void).
+It is called automatically when using the new keyword. */
+
+public static void main(String[] args) {
+
+        Student s1 = new Student();
+        //output: constructor called.
+    }
+
+
+}
+
+class Student {
+
+    String name;
+    int age;
+
+    // Constructor
+    Student() {
+        System.out.println("Constructor Called");
+    }
+}
+
